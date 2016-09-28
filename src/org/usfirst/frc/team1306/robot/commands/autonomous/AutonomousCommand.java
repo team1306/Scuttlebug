cac9580a0f2b58fa1e9c2621608e5b80ac084c1e
JacksonGoth2 <jacksongoth@gmail.com>
2016-09-27 19:35:18 -0500

    autonomouscommand.java smartdashboard test
    added smartdashboard string to test autonomous mode

--- src/org/usfirst/frc/team1306/robot/commands/autonomous/AutonomousCommand.java
+++ src/org/usfirst/frc/team1306/robot/commands/autonomous/AutonomousCommand.java
@@ -1,11 +1,14 @@
 package org.usfirst.frc.team1306.robot.commands.autonomous;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class AutonomousCommand extends CommandGroup{
 
 	public AutonomousCommand(Direction direction) {
 		
 		addSequential(direction.getDriveCommand());
+		SmartDashboard.putString("string", "test");
+		
 	}
 }
