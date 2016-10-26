713eb07ebb357b9e0bd6b8a72a3df6150dd78361
aaacool <amitrajesh200@gmail.com>
2016-10-25 20:37:18 -0500

    Added SmartQuickTurn logic

--- src/org/usfirst/frc/team1306/robot/commands/SmartQuickTurn.java
+++ src/org/usfirst/frc/team1306/robot/commands/SmartQuickTurn.java
@@ -2,6 +2,10 @@ package org.usfirst.frc.team1306.robot.commands;
 
 import org.usfirst.frc.team1306.robot.Constants;
 
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj.SerialPort.Port;
+
 /**
  * 
  * Quickly turns to a certain degree measure
@@ -15,14 +19,31 @@ public class SmartQuickTurn extends CommandBase {
 	final double speed = 0;
 	final double degree = 0;
 	boolean isDoneTurning = false;
+	AHRS ahrs= new AHRS(Port.kMXP);
 	
 	public SmartQuickTurn(boolean left, double degree) {
 		requires(drivetrain);
-		if(left == true) {
-			
-		} else {
+		ahrs= new AHRS(Port.kMXP);
+		double current_angle=ahrs.getAngle();
+		double angle_difference=current_angle-degree;
+		int done=0;
+
+		while (done==0) {
+			if (angle_difference == 0) {
+				done=1;
+			}
+			if (angle_difference < 0) {
+				drivetrain.tankDrive(-0.5, 0.5);
+			}
+			else {
+				drivetrain.tankDrive(0.5,-0.5);
+				
+			}
+		
+
 			
 		}
+
 	}
 	
 	@Override
